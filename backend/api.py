0e24e3c6b5a54deed6d12a6fe08cdd068b1a67e0
braedynl <braedynlea@gmail.com>
2026-09-15 18:28:13 -0400

    Add api.py functionality, Add placeholder markdown NOTE:api functionality has not been tested

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,42 @@
-#maybe change location
+import threading
+import socket
+#Re organize where this is when Rishi does a PR wioth his work!
+
+
+class TcpClient(threading.Thread):
+    def __init__(self, host, port):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.port = port
+        self.client = None
+
+    def listen(self):
+        reader = self.client.makefile("r", encoding="utf-8")
+        try:
+            for line in reader:
+                if not line:
+                    break
+                temp(line) # place holder for llm plugin function
+        finally:
+            self.client.close()
+
+    def connect(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.host, self.port))
+
+        threading.Thread(target=self.listen, daemon=True).start()
+
+    def send(self, text:str):
+        if not self.client:
+            return
+        if not text.endswith("\n"):
+            text = text + "\n"
+        self.client.send(text.encode("utf-8"))
+
+    def close(self):
+        if self.client:
+            self.client.close()
+
+
+def temp(line:str):
+    return line
