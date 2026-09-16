5d94b77f414564a705f680a07e0b36a1284e563f
braedynl <braedynlea@gmail.com>
2026-09-15 21:04:06 -0400

    Add text insertion

--- backend/Go-stuff/internal/insert.go
+++ backend/Go-stuff/internal/insert.go
@@ -1,3 +1,22 @@
 package internal
 
-//inserts text in detected box
+// inserts text in detected box
+
+import (
+	robot "github.com/go-vgo/robotgo"
+)
+
+type InsertionData struct {
+	text     string
+	length   int
+	approved bool // tab pressed or not, possibly useful data/telemetry whatever its called
+	relevant bool // thumbs up in gui = yes thumbs down = no, for gui/llm later
+}
+
+func findBox() (x, y int) { // needs to find where cursor is and return int/string of what box
+	return 1, 0 //temp
+}
+func Insert(text string, length int) {
+	robot.WriteAll(text)
+	robot.CmdV()
+}
